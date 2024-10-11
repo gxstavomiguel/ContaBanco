914dd489349e93530dabdab65a1153a53ba6f48a
Gustavo Miguel <gustavomiguelsilvafc@gmail.com>
2024-10-11 19:21:51 -0300

    Initial commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args){
+
+                Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+                System.out.println("Informe seu nome:");
+                String Nome = scanner.next();
+
+                System.out.println("Informe sua Agencia:");
+                int Agencia = scanner.nextInt();
+
+                System.out.println("Informe sua Conta:");
+                String Numero = scanner.next();
+
+                System.out.println("Informe seu saldo:");
+                double Saldo = scanner.nextDouble();
+
+                System.out.println("Olá, " + Nome + " obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + " conta " + Numero + " e seu saldo é de" +  Saldo + " e já está disponível para saque.");
+
+            }
+        }
